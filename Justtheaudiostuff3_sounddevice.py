1daa9b69a798da8230492232db36bb38c7379280
Markus <mkmuellner@gmail.com>
2024-08-26 23:04:24 +0200

    Update Justtheaudiostuff3_sounddevice.py

--- Justtheaudiostuff3_sounddevice.py
+++ Justtheaudiostuff3_sounddevice.py
@@ -141,13 +141,18 @@ stream = sd.OutputStream(callback=audio_callback, samplerate=fs, blocksize=ms_to
 print("Pre-filling grain queue...")
 while not grain_queue.full():  # Only fill if there's space in the queue
     try:
-        start_position = np.random.randint(0, len(data) - ms_to_samples(grain_size_ms, fs))
-        grain = generate_grain(data, data_reverse, start_position, ms_to_samples(grain_size_ms,fs), envelope_type=envelope_type, mix=mix, pitch=grain_pitch, pitch_variation=random_pitch_variation)
+        grain_size_samples = ms_to_samples(grain_size_ms, fs)
+        start_position = np.random.randint(0, len(data) - grain_size_samples)
+        grain = generate_grain(data, data_reverse, start_position, grain_size_samples, 
+                               envelope_type=envelope_type, mix=mix, pitch=grain_pitch, 
+                               pitch_variation=random_pitch_variation, apply_pitch=apply_pitch, 
+                               apply_envelope=apply_envelope, apply_random_pitch=apply_random_pitch, 
+                               apply_random_grain_size=apply_random_grain_size)
         grain_queue.put_nowait(grain)
     except queue.Full:
-        # If the queue is full, stop pre-filling
         print("Grain queue is full, stopping pre-fill.")
         break
+
 
 # Start a thread for keyboard handling, only if keyboard input is enabled
 if keyboard_input_enabled:
